c17b7cd847577312b921611ae354cf2a9315789e
Constantin Tuca <costi.tuca@gmail.com>
2024-09-02 17:02:38 +0300

    working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import ContactComponent from './contact/contact.component';
 import HomeComponent from './home/home.component';
+import BlogComponent from './blog/blog.component';
+import ContactComponent from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/blog/blog-card/blog-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-card.component.html',
+  styleUrls: ['./blog-card.component.css']
+})
+
+export default class BlogCardComponent {}
