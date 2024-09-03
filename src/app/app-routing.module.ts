9a74cb00c36c92267139137c2e58a3ecb5045594
Constantin Tuca <costi.tuca@gmail.com>
2024-09-03 09:33:22 +0300

    working on blog article view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import HomeComponent from './home/home.component';
 import BlogComponent from './blog/blog.component';
+import BlogArticleViewComponent from './blog/blog-article-view/blog-article-view.component';
 import ContactComponent from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'blog', component: BlogComponent },
+  { path: 'article-view', component: BlogArticleViewComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' }
 ];
--- src/app/blog/blog-card/blog-card.component.ts
+++ src/app/blog/blog-card/blog-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faFolder } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-blog-card',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog-card.component.css']
 })
 
-export default class BlogCardComponent {}
+export default class BlogCardComponent {
+  faFolder = faFolder;
+  message = `
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt vulputate metus non blandit. Donec luctus dapibus ornare. Duis sit amet odio eget nisl venenatis placerat eget eget diam. Maecenas mattis blandit mauris, id molestie felis posuere quis. Maecenas varius blandit consequat. Donec tincidunt dictum felis id egestas. Phasellus lorem turpis, vehicula quis enim vitae, commodo porta purus. Donec at pharetra lorem. </p>
+
+    <p>Quisque faucibus dolor nec felis euismod scelerisque. Maecenas sit amet auctor purus. Fusce imperdiet mattis egestas. Duis justo sapien, scelerisque quis ante vel, rutrum vehicula dui. Proin eget leo eu est vestibulum viverra. Etiam arcu tortor, convallis ac scelerisque nec, condimentum sed velit. </p>`
+}
