15bdc80760230a0aa7ea84931965f498e0c58f92
Constantin Tuca <costi.tuca@gmail.com>
2024-09-06 16:00:08 +0300

    continue working

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import HomeComponent from './client/home/home.component';
 import BlogComponent from './client/blog/blog.component';
 import BlogArticleViewComponent from './client/blog/blog-article-view/blog-article-view.component';
 import ContactComponent from './client/contact/contact.component';
+import ServicesComponent from './client/services/services.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'article-view', component: BlogArticleViewComponent },
   { path: 'contact', component: ContactComponent },
---
+++ client/src/app/client/services/services.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  standalone: true,
+  imports: [],
+  styleUrls: ['./services.component.css']
+})
+export default class ServicesComponent {}
